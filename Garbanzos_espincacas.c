cee32dcc05e5d53565b9a28ec792de6bda6ad850
RB <rodrigobedmar@gmail.com>
2020-07-13 17:39:31 -0400

    receta de garzanzos added

---
+++ Garbanzos_espincacas.c
@@ -0,0 +1,18 @@
+/*  Garbanzos con Espinacas
+    Cheakpeas with Spinach  */
+
+    ingredientes(personas * 2)
+    {
+        1 manojo Espinacas
+        1 lata Garbanzos
+        2 dientes de Ajo
+        1 Tbs EVOO
+        1 tbs Comino
+        1 tbs Salt
+        1/2 tbs pepper
+    }
+
+    procedimiento()
+    {
+        //TODO
+    }
